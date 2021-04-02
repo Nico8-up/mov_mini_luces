f3b374c7560b3969799ccac99f5971cd8f4f2741
Nico8-up <80417806+Nico8-up@users.noreply.github.com>
2021-04-02 12:30:20 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,9 +5,13 @@ input.onButtonPressed(Button.A, function () {
     pixel_array.range(3, 4).showColor(neopixel.colors(NeoPixelColors.Orange))
     pixel_array.range(4, 5).showColor(neopixel.colors(NeoPixelColors.Blue))
     pins.servoWritePin(AnalogPin.P1, 180)
-    pins.servoWritePin(AnalogPin.P2, 180)
+    pins.servoWritePin(AnalogPin.P2, 0)
 })
 input.onButtonPressed(Button.AB, function () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
+})
+input.onSound(DetectedSound.Loud, function () {
     pins.servoWritePin(AnalogPin.P1, 90)
     pins.servoWritePin(AnalogPin.P2, 90)
 })
@@ -15,7 +19,7 @@ input.onButtonPressed(Button.B, function () {
     pixel_array.clear()
     pixel_array.show()
     pins.servoWritePin(AnalogPin.P1, 0)
-    pins.servoWritePin(AnalogPin.P2, 0)
+    pins.servoWritePin(AnalogPin.P2, 180)
 })
 let pixel_array: neopixel.Strip = null
 pixel_array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
